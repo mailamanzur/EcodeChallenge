8c01336fae7246e6a22fdd0f586b6432bb993a4a
mailamanzur <mailamanzur@gmail.com>
2017-08-23 01:37:14 +0200

    Initial Commit

---
+++ eyCodeChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eyCodeChallenge
+//
+//  Created by Maila  Manzur on 23/08/2017.
+//  Copyright © 2017 Maila  Manzur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ eyCodeChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  eyCodeChallenge
+//
+//  Created by Maila  Manzur on 23/08/2017.
+//  Copyright © 2017 Maila  Manzur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
